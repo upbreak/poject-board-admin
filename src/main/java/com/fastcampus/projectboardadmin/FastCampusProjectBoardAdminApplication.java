c5fefe609bb4ea783d1b67f224e4c3b948b042f2
jinwoo <upbreak05@kakao.com>
2024-01-25 12:18:30 +0900

    #1 - 스프링 부트 프로젝트 초기화
    스프링 부트 뼈대 작성

---
+++ src/main/java/com/fastcampus/projectboardadmin/FastCampusProjectBoardAdminApplication.java
@@ -0,0 +1,16 @@
+package com.fastcampus.projectboardadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@ConfigurationPropertiesScan
+@SpringBootApplication
+public class FastCampusProjectBoardAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastCampusProjectBoardAdminApplication.class, args);
+    }
+
+
+}
