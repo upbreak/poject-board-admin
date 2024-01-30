52f9744dce856bb35d059b0c589a09dafc6bfe53
jinwoo <upbreak05@kakao.com>
2024-01-30 13:43:45 +0900

    회원 정보에 접근할 repository 인터페이스, jpa 설정 및 jp 테스트 작성
    회원 데이터에 접근할 리포지토리 인터페이스와
    jpa auditing 기능을 활성화시킬 `JpaConfig`,
    각자 다른 권한을 가진 테스트용 회원 데이터,
    crud 쿼리 생성을 확인할 jpa 테스트 작성

---
+++ src/main/java/com/fastcampus/projectboardadmin/config/JpaConfig.java
@@ -0,0 +1,27 @@
+package com.fastcampus.projectboardadmin.config;
+
+import com.fastcampus.projectboardadmin.dto.security.BoardAdminPrincipal;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return () -> Optional.ofNullable(SecurityContextHolder.getContext())//SecurityContextHolder - 세큐리티의 모든 정보를 가지는 클래스
+                .map(SecurityContext::getAuthentication)//인증정보 확인
+                .filter(Authentication::isAuthenticated)//인증 되었는지 확인
+                .map(Authentication::getPrincipal)//로그인 정보 가져오기
+                .map(BoardAdminPrincipal.class::cast)//커스텀 principal로 캐스트
+                .map(BoardAdminPrincipal::getUsername);
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+-- 유저
+insert into user_account (user_id, user_password, role_types, nickname, email, memo, create_at, create_by, modified_at, modified_by) values
+('jinwoo', '{noop}asdf1234', 'ADMIN', 'Jinwoo', 'jinwoo@mail.com', 'I am Jinwoo.', now(), 'jinwoo', now(), 'jinwoo'),
+('mark', '{noop}asdf1234', 'MANAGER', 'Mark', 'mark@mail.com', 'I am Mark.', now(), 'jinwoo', now(), 'jinwoo'),
+('susan', '{noop}asdf1234', 'MANAGER,DEVELOPER', 'Susan', 'Susan@mail.com', 'I am Susan.', now(), 'jinwoo', now(), 'jinwoo'),
+('jim', '{noop}asdf1234', 'USER', 'Jim', 'jim@mail.com', 'I am Jim.', now(), 'jinwoo', now(), 'jinwoo')
+;
