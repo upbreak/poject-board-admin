e2f2ea2604c719a0d2f12e56fc992dcbad599377
jinwoo <upbreak05@kakao.com>
2024-02-02 15:58:53 +0900

    #12 - 게시글 관리 페이지에 템플릿 레이아웃 및 DataTable 적용
    게시글 관리 페이지가 제대로 표현되도록 미리 준비해 둔
    공통 레이아웃을 적용하고, 테이블 요소를 추가
    이 때 자바스크립트 DataTable 플러그인 사용
    이를 위해 필요한 추가적인 스타일시트, 자바스크립트 리소스를 등록
    
    *** 스프링부트3 부터는 타임리프에서 보안상 이유로
    #request, #response 등의 객체들을 직접 사용할 수 없도록 변경이 되었다.
    해서 컨트롤러에서 모델에 HttpServletRequest를 이용하여 직접 넘겨주는 방식으로 처리하였다.

--- src/main/java/com/fastcampus/projectboardadmin/controller/ArticleManagementController.java
+++ src/main/java/com/fastcampus/projectboardadmin/controller/ArticleManagementController.java
@@ -1,5 +1,6 @@
 package com.fastcampus.projectboardadmin.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -16,7 +17,9 @@ public class ArticleManagementController {
     public String articles(
             @PageableDefault(size = 10, sort = "createAt", direction = Sort.Direction.DESC)Pageable pageable
             , Model model
+            , HttpServletRequest request
             ){
+        model.addAttribute("requestURI", request.getRequestURI());
         return "management/articles";
     }
 }
