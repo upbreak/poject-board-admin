e4f657769c2d5b652906c0aeaea4fe0037d9e872
jinwoo <upbreak05@kakao.com>
2024-02-06 16:23:36 +0900

    #28 - 어드민 회원 컨트롤러에 필요한 핸들러 메소드 및 테스트

--- src/main/java/com/fastcampus/projectboardadmin/controller/AdminAccountController.java
+++ src/main/java/com/fastcampus/projectboardadmin/controller/AdminAccountController.java
@@ -1,27 +1,46 @@
 package com.fastcampus.projectboardadmin.controller;
 
+import com.fastcampus.projectboardadmin.dto.reponse.AdminAccountResponse;
+import com.fastcampus.projectboardadmin.service.AdminAccountService;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequestMapping("/admin/members")
 @Controller
 public class AdminAccountController {
 
+    private AdminAccountService adminAccountService;
+
     @GetMapping
     public String members(
-            @PageableDefault(size = 10, sort = "createAt", direction = Sort.Direction.DESC) Pageable pageable
-            , Model model
+            Model model
             , HttpServletRequest request
     ){
         model.addAttribute("requestURI", request.getRequestURI());
 
         return "admin/members";
+    }
+
+    @ResponseBody
+    @GetMapping("/api/admin/members")
+    public List<AdminAccountResponse> getMembers() {
+        return List.of();
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseBody
+    @DeleteMapping("/api/admin/members/{userId}")
+    public void delete(@PathVariable String userId){
+
     }
 }
